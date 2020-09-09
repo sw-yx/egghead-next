9a9b9bf277c139fdfc0948aecb7623d2261d4ce4
Maggie Appleton <maggie.fm.appleton@gmail.com>
2020-09-09 18:37:48 +0100

    setup image scroller component

---
+++ src/components/mdx/ImageScroller.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import gsap from 'gsap'
+// import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+// registerPlugin(ScrollTrigger)
+
+export default function ImageScroller({children}) {
+  useEffect(() => {
+    // effect
+  }, [])
+
+  return <div>{children}</div>
+}
