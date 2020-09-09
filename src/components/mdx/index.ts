12ab801733dcb195ac5b7bc545b8229ffcb43c52
Maggie Appleton <maggie.fm.appleton@gmail.com>
2020-09-09 18:38:13 +0100

    added new msx components to main index file

--- src/components/mdx/index.ts
+++ src/components/mdx/index.ts
@@ -1,9 +1,15 @@
 import Course from './course'
 import Link from 'next/link'
+import CodeBox from './codeBox'
+import ImageScroller from './ImageScroller'
+import Callout from './Callout'
 
 const mdxComponents = {
   Course,
   Link,
+  CodeBox,
+  ImageScroller,
+  Callout,
 }
 
 export default mdxComponents
