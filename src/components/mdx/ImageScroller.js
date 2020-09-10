2b2596e8538c73de58a62e7f290ad1540dd87833
Maggie Appleton <maggie.fm.appleton@gmail.com>
2020-09-10 16:13:52 +0100

    experimental gsap image scroller component

--- src/components/mdx/ImageScroller.js
+++ src/components/mdx/ImageScroller.js
@@ -1,13 +1,54 @@
-import React from 'react'
-import gsap from 'gsap'
-// import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import React, {useRef, useState, useEffect} from 'react'
+import {gsap} from 'gsap'
+// import { ScrollTrigger } from "gsap/ScrollTrigger"
 
-// registerPlugin(ScrollTrigger)
+// gsap.registerPlugin(ScrollTrigger)
 
 export default function ImageScroller({children}) {
+  const imageRef = useRef(null)
+  // const [image, setImage] = useState('')
+
   useEffect(() => {
-    // effect
+    const timeline = gsap.timeline({
+      // scrollTrigger: {
+      //     trigger: '.triggerElement',
+      //     scrub: true
+      // }
+    })
+
+    timeline
+      .to([imageRef.current], 1, {
+        attr: {
+          src:
+            'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1599594005/maggieappleton.com/notes/gsap/GSAP-DOM.jpg',
+        },
+      })
+      .to([imageRef.current], 1, {
+        attr: {
+          src:
+            'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1599594005/maggieappleton.com/notes/gsap/GSAP-DOM-2.jpg',
+        },
+      })
+      .to([imageRef.current], 1, {
+        attr: {
+          src:
+            'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1599594005/maggieappleton.com/notes/gsap/GSAP-DOM-3.jpg',
+        },
+      })
+      .to([imageRef.current], 1, {
+        attr: {
+          src:
+            'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1599594005/maggieappleton.com/notes/gsap/GSAP-DOM-4.jpg',
+        },
+      })
   }, [])
 
-  return <div>{children}</div>
+  return (
+    <div className="triggerElement">
+      <img
+        ref={imageRef}
+        rc="https://res.cloudinary.com/dg3gyk0gu/image/upload/v1599594005/maggieappleton.com/notes/gsap/GSAP-DOM.jpg"
+      />
+    </div>
+  )
 }
