4aa50db43ddfcc8a2d40251185d382a715fb289a
Maggie Appleton <maggie.fm.appleton@gmail.com>
2020-09-11 11:24:06 +0100

    reversed template styling after updating main css files

--- src/layouts/ultimate-guide.tsx
+++ src/layouts/ultimate-guide.tsx
@@ -30,7 +30,7 @@ const UltimateGuideLayout: FunctionComponent<LayoutProps> = ({
         }}
         canonical={url}
       />
-      <div className="prose max-w-2xl text-xl">
+      <div className="prose max-w-none text-xl">
         <h1>{title}</h1>
         {children}
       </div>
