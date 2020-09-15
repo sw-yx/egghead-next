e9fb1a5239608346915b3f66e7356334899a2be2
Maggie Appleton <maggie.fm.appleton@gmail.com>
2020-09-15 21:55:39 +0100

    got scrolltrigger working, janky image loading still broken

--- src/pages/learn/javascript/fundamentals/the-dom/DOMScroller.js
+++ src/pages/learn/javascript/fundamentals/the-dom/DOMScroller.js
@@ -1,44 +1,59 @@
 import React, {useRef, useState, useEffect} from 'react'
-import {gsap} from 'gsap'
-// import { ScrollTrigger } from "gsap/ScrollTrigger"
+import {gsap} from 'gsap/dist/gsap'
+import {ScrollTrigger} from 'gsap/dist/ScrollTrigger'
 
-// gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger)
 
 export default function DOMScroller() {
   const imageRef = useRef(null)
-  // const [image, setImage] = useState('')
+  // const [isLoading, setIsLoading] = useState(true)
+  // const [images, setImages] = useState([])
+
+  const altText =
+    'The DOM is the “final output” of your browser after reading the HTML'
 
   useEffect(() => {
     const timeline = gsap.timeline({
-      repeat: 5,
-      // scrollTrigger: {
-      //     trigger: '.triggerElement',
-      //     scrub: true
-      // }
+      scrollTrigger: {
+        trigger: '#triggerElement',
+        start: 'top 50%',
+        end: 'bottom 80%',
+        markers: true,
+        scrub: true,
+      },
     })
 
     timeline
       .to([imageRef.current], {
         attr: {
           src:
-            'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1599812699/egghead-next-ebombs/wtf-DOM/dom-2.png',
+            'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1599816716/egghead-next-ebombs/wtf-DOM/dom2.jpg',
         },
-        duration: 2,
       })
       .to([imageRef.current], {
         attr: {
           src:
-            'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1599812699/egghead-next-ebombs/wtf-DOM/dom-3.png',
+            'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1599816717/egghead-next-ebombs/wtf-DOM/dom3.jpg',
         },
-        duration: 4,
+      })
+      .to([imageRef.current], {
+        attr: {
+          src:
+            'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1600084432/egghead-next-ebombs/wtf-DOM/dom4.jpg',
+        },
       })
   }, [])
 
   return (
-    <div className="triggerElement">
+    <div
+      style={{width: '100%', height: '700px', marginBottom: '10%'}}
+      id="triggerElement"
+    >
       <img
+        style={{maxWidth: '600px', maxHeight: '100%', margin: '0 auto'}}
         ref={imageRef}
-        src="https://res.cloudinary.com/dg3gyk0gu/image/upload/v1599812699/egghead-next-ebombs/wtf-DOM/dom-2.png"
+        src="https://res.cloudinary.com/dg3gyk0gu/image/upload/v1599817004/egghead-next-ebombs/wtf-DOM/dom1.jpg"
+        alt={altText}
       />
     </div>
   )
