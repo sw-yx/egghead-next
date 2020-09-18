03402725234c85def3465a372ac46ca1e589af31
Maggie Appleton <maggie.fm.appleton@gmail.com>
2020-09-18 21:42:09 +0100

    Fixed up node scroller

--- src/pages/learn/javascript/fundamentals/the-dom/NodesScroller.js
+++ src/pages/learn/javascript/fundamentals/the-dom/NodesScroller.js
@@ -5,7 +5,8 @@ import {ScrollTrigger} from 'gsap/dist/ScrollTrigger'
 gsap.registerPlugin(ScrollTrigger)
 
 export default function NodesScroller() {
-  const triggerRef = useRef(null)
+  const pinRef = useRef(null)
+  const textRef = useRef(null)
   const nodes1 = useRef(null)
   const nodes2 = useRef(null)
   const nodes3 = useRef(null)
@@ -15,24 +16,30 @@ export default function NodesScroller() {
     'The nodes in the DOM tree are like a family tree. They have parents, children, and sibling nodes'
 
   const textSteps = [
-    'Each element in the HTML like paragraphs, images, and divs are represented by “nodes.”',
-    'The nodes have relationships with other nodes, and can have “parents,” “children,” and “siblings.',
-    'You can think of these relationships as you would with any family tree.',
-    'Children” of the same “Parent” are “Siblings.',
+    'Each element in the HTML like paragraphs, images, and divs are represented by “nodes.” The nodes have relationships with other nodes, and can have “parents,” “children,” and “siblings.',
+    'You can think of these relationships as you would with any family tree. "Children” of the same “Parent” are “Siblings."',
   ]
 
   useEffect(() => {
-    const timeline = gsap.timeline({
+    const scrollTimeline = gsap.timeline({
       scrollTrigger: {
-        trigger: [triggerRef.current],
+        trigger: [pinRef.current],
         start: 'top top',
         scrub: true,
-        pin: [triggerRef.current],
+        pin: [pinRef.current],
         pinSpacing: true,
       },
     })
 
-    timeline
+    const labelsTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: [textRef.current],
+        scrub: true,
+        start: 'top 70%',
+      },
+    })
+
+    labelsTimeline
       .to([nodes1.current], {
         opacity: 1,
       })
@@ -54,61 +61,103 @@ export default function NodesScroller() {
       .to([nodes4.current], {
         opacity: 1,
       })
+
+    scrollTimeline.add(labelsTimeline)
   }, [])
 
   return (
     <div
-      ref={triggerRef}
       style={{
-        width: '100%',
-        height: '960px',
+        width: '130%',
+        marginLeft: '-15%',
+        height: '130vh',
         display: 'flex',
         flexDirection: 'row',
-        paddingTop: '2em',
+        paddingTop: '1em',
+        marginTop: '-3%',
+        marginBottom: '105vh',
       }}
     >
       <div
+        ref={pinRef}
         style={{
-          width: '30%',
+          width: '60%',
+          position: 'relative',
           display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'start',
-          marginRight: '1.6em',
+          justifyContent: 'flex-end',
         }}
       >
-        {textSteps.map((step) => {
-          return <p>{step}</p>
-        })}
-      </div>
-      <div style={{width: '65%', position: 'relative'}}>
         <img
           alt={altText}
-          style={{position: 'absolute', top: 0, left: 0, zIndex: 0}}
+          style={{maxHeight: '90vh', position: 'absolute', top: 0, zIndex: 0}}
           src="https://res.cloudinary.com/dg3gyk0gu/image/upload/v1600099812/egghead-next-ebombs/wtf-DOM/dom-tree.jpg"
         />
         <img
           ref={nodes1}
-          style={{position: 'absolute', top: 0, left: 0, opacity: 0, zIndex: 1}}
+          alt={altText}
+          style={{
+            maxHeight: '90vh',
+            position: 'absolute',
+            top: 0,
+            opacity: 0,
+            zIndex: 1,
+          }}
           src="https://res.cloudinary.com/dg3gyk0gu/image/upload/v1600099798/egghead-next-ebombs/wtf-DOM/dom-nodes-1.png"
         />
 
         <img
           ref={nodes2}
-          style={{position: 'absolute', top: 0, left: 0, opacity: 0, zIndex: 2}}
+          alt={altText}
+          style={{
+            maxHeight: '90vh',
+            position: 'absolute',
+            top: 0,
+            opacity: 0,
+            zIndex: 2,
+          }}
           src="https://res.cloudinary.com/dg3gyk0gu/image/upload/v1600099804/egghead-next-ebombs/wtf-DOM/dom-nodes-2.png"
         />
 
         <img
           ref={nodes3}
-          style={{position: 'absolute', top: 0, left: 0, opacity: 0, zIndex: 1}}
+          alt={altText}
+          style={{
+            maxHeight: '90vh',
+            position: 'absolute',
+            top: 0,
+            opacity: 0,
+            zIndex: 1,
+          }}
           src="https://res.cloudinary.com/dg3gyk0gu/image/upload/v1600099804/egghead-next-ebombs/wtf-DOM/dom-nodes-3.png"
         />
 
         <img
           ref={nodes4}
-          style={{position: 'absolute', top: 0, left: 0, opacity: 0, zIndex: 2}}
+          alt={altText}
+          style={{
+            maxHeight: '90vh',
+            position: 'absolute',
+            top: 0,
+            opacity: 0,
+            zIndex: 2,
+          }}
           src="https://res.cloudinary.com/dg3gyk0gu/image/upload/v1600099804/egghead-next-ebombs/wtf-DOM/dom-nodes-4.png"
         />
+      </div>
+      <div
+        ref={textRef}
+        style={{
+          width: '40%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'end',
+          paddingLeft: '4em',
+          marginTop: '80vh',
+        }}
+      >
+        {textSteps.map((step) => {
+          return <p style={{marginBottom: '50vh'}}>{step}</p>
+        })}
       </div>
     </div>
   )
