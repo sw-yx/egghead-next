ecd6a92c4b6c1b3b0e9d7e7172964b737019f321
Maggie Appleton <maggie.fm.appleton@gmail.com>
2020-09-21 07:30:31 +0100

    cleaned up tester mdx components

--- src/components/mdx/CodeBox.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const container = ''
-
-export default function CodeBox({children}) {
-  return <div className={container}>{children}</div>
-}
--- src/components/mdx/index.ts
+++ src/components/mdx/index.ts
@@ -1,12 +1,10 @@
 import Course from './course'
 import Link from 'next/link'
-import CodeBox from './codeBox'
 import Callout from './Callout'
 
 const mdxComponents = {
   Course,
   Link,
-  CodeBox,
   Callout,
 }
 
